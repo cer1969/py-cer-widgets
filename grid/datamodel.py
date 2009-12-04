57300dceb1548d088d67145097bc92145c085d8f
Cristian Echeverria <cer1969@gmail.com>
2009-12-04 14:26:11 -0300

    Cambios en grid.Header

--- grid/datamodel.py
+++ grid/datamodel.py
@@ -6,16 +6,17 @@ import wx.grid as grid
 
 #-----------------------------------------------------------------------------------------
 
-__all__ = ['Header', 'DataModel', 'TableDataModel',
+__all__ = ['Header', 'Text', 'Float', 'Int', 'Bool', 'Choice',
+           'DataModel', 'TableDataModel',
            'VALUE_STRING', 'VALUE_FLOAT', 'VALUE_NUMBER', 'VALUE_BOOL', 'VALUE_CHOICE'
            ]
 
 #-----------------------------------------------------------------------------------------
 
 VALUE_STRING = grid.GRID_VALUE_STRING
-VALUE_FLOAT = grid.GRID_VALUE_FLOAT
+VALUE_FLOAT  = grid.GRID_VALUE_FLOAT
 VALUE_NUMBER = grid.GRID_VALUE_NUMBER
-VALUE_BOOL = grid.GRID_VALUE_BOOL
+VALUE_BOOL   = grid.GRID_VALUE_BOOL
 VALUE_CHOICE = grid.GRID_VALUE_CHOICE
 
 #-----------------------------------------------------------------------------------------
@@ -36,22 +37,32 @@ DEFAULT[VALUE_CHOICE] = (str,"")
 # Class Header
 
 class Header(object):
-    def __init__(self,name,type=VALUE_STRING,
-                           defval=None,
-                           func=None,
-                           args=None):
-
+    def __init__(self, name, type=VALUE_STRING, defval=None, func=None, args=None):
         self.name   = name
         self.type   = type
-
-        if defval is None:
-            defval = DEFAULT[self.type][1]
-        if func is None:
-            func = DEFAULT[self.type][0]
-
-        self.defval = defval
-        self.func   = func
+        self.defval = DEFAULT[type][1] if (defval is None) else defval
+        self.func   = DEFAULT[type][0] if (func is None) else func
         self.args   = args
+
+class Text(Header):
+    def __init__(self, name, defval=None, func=None, args=None):
+        Header.__init__(self, name, VALUE_STRING, defval, func, args)
+
+class Float(Header):
+    def __init__(self, name, defval=None, func=None, args=None):
+        Header.__init__(self, name, VALUE_FLOAT, defval, func, args)
+
+class Int(Header):
+    def __init__(self, name, defval=None, func=None, args=None):
+        Header.__init__(self, name, VALUE_NUMBER, defval, func, args)
+
+class Bool(Header):
+    def __init__(self, name, defval=None, func=None, args=None):
+        Header.__init__(self, name, VALUE_BOOL, defval, func, args)
+
+class Choice(Header):
+    def __init__(self, name, defval=None, func=None, args=None):
+        Header.__init__(self, name, VALUE_CHOICE, defval, func, args)
 
 #-----------------------------------------------------------------------------------------
 # Class DataModel
@@ -59,51 +70,51 @@ class Header(object):
 class DataModel(grid.PyGridTableBase):
     def __init__(self):
         grid.PyGridTableBase.__init__(self)
-
+    
     #-------------------------------------------------------------------------------------
     # Métodos requeridos por wx.PyGridTableBase
-
+    
     def GetNumberRows(self):
         return 10
-
+    
     def GetNumberCols(self):
         return 10
-
-    def IsEmptyCell(self,row,col):
+    
+    def IsEmptyCell(self, row, col):
         return False
-
-    def GetValue(self,row,col):
-        return "%d,%d" % (row,col)
-
+    
+    def GetValue(self, row, col):
+        return "%d,%d" % (row, col)
+    
     def SetValue(self, row, col, value):
         # Se debe sobreescribir
         pass
-
+    
     def GetColLabelValue(self, col):
         return "Columna %d" % col
-
+    
     def GetRowLabelValue(self, row):
         return "%d" % row
-
+    
     # Llamada para determinar tipo de editor/renderer a usar
-    def GetTypeName(self,row,col):
+    def GetTypeName(self, row, col):
         return VALUE_STRING
-
+    
     # Llamada para determinar como la data puede ser obtenida y guardada por el
     # editor/renderer.
-    def CanGetValueAs(self,row,col,typeName):
-        colType = self.GetTypeName(row,col).split(':')[0]
+    def CanGetValueAs(self, row, col, typeName):
+        colType = self.GetTypeName(row, col).split(':')[0]
         if typeName == colType:
             return True
         else:
             return False
-
-    def CanSetValueAs(self,row,col,typeName):
-        return self.CanGetValueAs(row,col,typeName)
-
+    
+    def CanSetValueAs(self, row, col, typeName):
+        return self.CanGetValueAs(row, col, typeName)
+    
     #-------------------------------------------------------------------------------------
     # Métodos Cer para SobreEscribir
-
+    
     def ValidateData(self):
         return True
 
@@ -114,86 +125,86 @@ class DataModel(grid.PyGridTableBase):
 
 class TableDataModel(DataModel):
     
-    def __init__(self,headers=[]):
+    def __init__(self, headers=[]):
         DataModel.__init__(self)
         self.Headers = headers
         self.__data = []
-
+    
     #-------------------------------------------------------------------------------------
     # Propiedad data
     
     def GetData(self):
         return self.__data
-        
-    def SetData(self,data):
+    
+    def SetData(self, data):
         data = self.GetCheckedData(data)
         self.__data = data
-
-    Data = property(GetData,SetData)
-
+    
+    Data = property(GetData, SetData)
+    
     #-------------------------------------------------------------------------------------
     # Métodos SobreEscritos de DataModel
-
+    
     def GetNumberRows(self):
         return len(self.Data)
-
+    
     def GetNumberCols(self):
         return len(self.Headers)
-
-    def IsEmptyCell(self,row,col):
+    
+    def IsEmptyCell(self, row, col):
         return not self.Data[row][col]
-
-    def GetValue(self,row,col):
+    
+    def GetValue(self, row, col):
         return self.Data[row][col]
-
-    def SetValue(self,row,col,value):
+    
+    def SetValue(self, row, col, value):
         self.Data[row][col] = value
-
-    def GetColLabelValue(self,col):
+    
+    def GetColLabelValue(self, col):
         header = self.Headers[col]
         return header.name
     
-    def GetTypeName(self,row,col):
+    def GetTypeName(self, row, col):
         header = self.Headers[col]
         typeName = header.type
         if not(header.args is None):
-            typeName = "%s:%s" % (typeName,header.args)
+            typeName = "%s:%s" % (typeName, header. args)
         return typeName
-
+    
     #--------------------------------------------------
     
-    def AppendRows(self,numRows=1):
+    def AppendRows(self, numRows=1):
         defValues = [header.defval for header in self.Headers]
         data = numRows*[defValues]
         for i in data:
             self.__data.append(i)
-
-    def DeleteRows(self,pos=0,numRows=1):
+    
+    def DeleteRows(self, pos=0, numRows=1):
         del(self.__data[pos:pos+numRows])
-
-    def InsertRows(self,pos=0,numRows=1):
+    
+    def InsertRows(self, pos=0, numRows=1):
         defValues = [header.defval for header in self.Headers]
         data = numRows*[defValues]
         for i in data:
             self.__data.insert(pos,i)
-
+    
     #--------------------------------------------------
     # Métodos CER
-
-    def AppendRowsData(self,data):
+    
+    def AppendRowsData(self, data):
         data = self.GetCheckedData(data)
         for i in data:
             self.__data.append(i)
-
-    def InsertRowsData(self,pos,data):
+    
+    def InsertRowsData(self, pos, data):
         data = self.GetCheckedData(data)
         #numRows = len(data)
         self.__data[pos:pos] = data
         #data.reverse()
         #for i in data:
         #    self.__data.insert(pos,i)
-
-    def GetCheckedData(self,data):
+    
+    def GetCheckedData(self, data):
         ncols = self.GetNumberCols()
         sal = []
         for row in data:
